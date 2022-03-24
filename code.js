98f202babe879326089940f3c5d49b6e810f527f
ethanosorio <101898908+ethanosorio@users.noreply.github.com>
2022-03-23 23:02:20 -0500

    testing spotify link

---
+++ code.js
@@ -0,0 +1,12 @@
+/*let button = document.getElementById("spotifyLink");
+button.addEventListener("change", () => {
+	document.getElementById("spotify").style.visibility = visible;
+	});
+}*/
+
+const element = document.getElementById("fa fa-spotify");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.querySelector("spotify").style.visiiblity = hidden;;
+}
